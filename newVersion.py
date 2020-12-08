5f1b070fde3ea77b763493c8c24e530163b3a995
BenFrancis <bneto@computacao.cua.ufmt.br>
2020-12-08 16:46:49 -0300

    ...

--- newVersion.py
+++ newVersion.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
-%matplotlib inline
+#%matplotlib inline
 
 
 dados = pd.read_csv('https://raw.githubusercontent.com/ChernoBen/IAatividadeII/main/new_dataset',
