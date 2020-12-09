0caf15c78e8c31d68ae5dca877543cac453bcfaa
benjamim <bneto@computacao.cua.ufmt.br>
2020-12-09 09:14:47 -0300

    finalização

--- LG_Covid19_BRASIL.py
+++ LG_Covid19_BRASIL.py
@@ -15,17 +15,24 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+
+dados = pd.read_csv('C:/Users/Benjamim/Desktop/bases/INFLUD-30-11-2020.csv',sep= ';')
+
 '''
-dados = pd.read_csv('C:/Users/Benjamim/Desktop/classificador/INFLUD-30-11-2020.csv',sep= ';')
-
-
 dados = pd.read_csv('https://raw.githubusercontent.com/ChernoBen/IAatividadeII/main/new_dataset',
                  sep= ';')'''
+
+
+#Tratamento de dados da base original
 df = dados[['NU_IDADE_N','FATOR_RISC','EVOLUCAO']]
 df.head()
-#removendo valores Nan
+
+#removendo valores Nan (not a number)
 df = df.dropna()
+#Lista de indices para melhor manipulação de variaveis da base
 dicio = ['EVOLUCAO']
+
+#rolutador binario de intancias para matrizes, np.array's e etc. 
 def rotula(dataset,param):
     arr =[]
     param = param
@@ -37,55 +44,54 @@ def rotula(dataset,param):
             arr.append(0)
     return arr
 for indice in range(len(dicio)):
-    #para cada valor na posição INDICE remova valores diferentes de positivo e negativo para os casos
+    # EM INDICE remova valores diferentes de positivo e negativo para os casos
     df  = df.drop(df[df[dicio[indice]] > 2 ].index)
     df  = df.drop(df[df[dicio[indice]] < 1  ].index)
     df[dicio[indice]] = rotula(df[dicio[indice]],1)
+    
 
-#removendo valores Nan
-df = df.dropna()
 #rotulando lados 1 == cura
 df['FATOR_RISC'] = rotula(df['FATOR_RISC'],'S')
 
+#Divisão da base entre treino e teste
 X = df[['NU_IDADE_N','FATOR_RISC']].values
 y = df['EVOLUCAO'].values
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state=42)
-
 X_test
 
-# instancia o classificador com nome logit
-logit = LogisticRegression()
-
+#Regressão logistica
+# instancia o classificador 
+classificador = LogisticRegression()
 # treina o modelo
-logit.fit(X_train, y_train)
-
+classificador.fit(X_train, y_train)
 # faz predicao e salva em y_pred
-logit.predict(X_test)
-y_pred = logit.predict(X_test)
-
+classificador.predict(X_test)
+y_pred = classificador.predict(X_test)
 # acuracia
-acuracia = logit.score(X_test, y_test)
-
+acuracia = classificador.score(X_test, y_test)
 # matriz de confusao
 print(confusion_matrix(y_test, y_pred))
 matriz_de_confusao = confusion_matrix(y_test, y_pred)
 
 # computa probabilidades
-y_pred_prob = logit.predict_proba(X_test)[:,1]
+y_pred_prob = classificador.predict_proba(X_test)[:,1]
 
-# Gera fpr, tpr e thresholds
-fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+novaBase = pd.DataFrame(X_test)
+novaBase['Result'] = y_test
 
-# curva ROC
-plt.plot([0, 1], [0, 1], 'k--')
-plt.plot(fpr, tpr)
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('ROC Curve')
-plt.show()
-sns.set_theme(style="darkgrid")
+sns.pairplot(novaBase,hue='Result');
+sns.pairplot(novaBase, hue = 'Result', diag_kind = 'kde',plot_kws = {'alpha': 0.6, 's': 80, 'edgecolor': 'k'},size = 4);
+plt.suptitle('Evolução de pacientes com covide Idade X Fator de risco', 
+             size = 15);
 
+sns.scatterplot(data=novaBase, x=0, y=1, hue='Result');
+
+
+#sigmoide
+def sigmoide(x):
+    return 1/(1+np.exp(-x))
+
+retaSigma = sigmoide(X_test * classificador.coef_ + classificador.intercept_).ravel()
 
 
 
