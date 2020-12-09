601f8d6ce4766a85556e967f680a91940e124a77
benjamim <bneto@computacao.cua.ufmt.br>
2020-12-09 10:20:42 -0300

    formatação

--- LG_Covid19_BRASIL.py
+++ LG_Covid19_BRASIL.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec  8 21:25:03 2020
+Created on Tue Nov  25 21:25:03 2020
 
 @author: Benjamim
 """
@@ -9,30 +9,29 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.metrics import roc_curve
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-
 dados = pd.read_csv('C:/Users/Benjamim/Desktop/bases/INFLUD-30-11-2020.csv',sep= ';')
-
-'''
-dados = pd.read_csv('https://raw.githubusercontent.com/ChernoBen/IAatividadeII/main/new_dataset',
-                 sep= ';')'''
-
-
 #Tratamento de dados da base original
 df = dados[['NU_IDADE_N','FATOR_RISC','EVOLUCAO']]
 df.head()
 
 #removendo valores Nan (not a number)
+#a) Remoção de casos de SRAG não diagnosticados como COVID-19
 df = df.dropna()
 #Lista de indices para melhor manipulação de variaveis da base
 dicio = ['EVOLUCAO']
-
-#rolutador binario de intancias para matrizes, np.array's e etc. 
+#rolutador binario de intancias para matrizes, np.array's e etc.
+#b)Remoção de variáveis relacionadas ao óbito (data do óbito e número da declaração de óbito)
+'''
+1-cura
+2-obito
+3-obito outras causas
+9-ignorado
+''' 
 def rotula(dataset,param):
     arr =[]
     param = param
@@ -47,46 +46,52 @@ for indice in range(len(dicio)):
     # EM INDICE remova valores diferentes de positivo e negativo para os casos
     df  = df.drop(df[df[dicio[indice]] > 2 ].index)
     df  = df.drop(df[df[dicio[indice]] < 1  ].index)
-    df[dicio[indice]] = rotula(df[dicio[indice]],1)
-    
-
+    df[dicio[indice]] = rotula(df[dicio[indice]],1)   
 #rotulando lados 1 == cura
 df['FATOR_RISC'] = rotula(df['FATOR_RISC'],'S')
 
-#Divisão da base entre treino e teste
+
+#c) Seleção e tratamento de variáveis;
 X = df[['NU_IDADE_N','FATOR_RISC']].values
 y = df['EVOLUCAO'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30, random_state=42)
-X_test
 
+#d) Treino de classificador para classificar entre “óbito por COVID-19” e “cura” (informação da coluna “EVOLUCAO”);
 #Regressão logistica
 # instancia o classificador 
 classificador = LogisticRegression()
 # treina o modelo
 classificador.fit(X_train, y_train)
-# faz predicao e salva em y_pred
+# faz predicao e salva em previoes
 classificador.predict(X_test)
-y_pred = classificador.predict(X_test)
+previsoes = classificador.predict(X_test)
+
+#e) Avaliar classificador e reportar a acurácia geral e por classe em conjunto de teste.
 # acuracia
 acuracia = classificador.score(X_test, y_test)
-# matriz de confusao
-print(confusion_matrix(y_test, y_pred))
-matriz_de_confusao = confusion_matrix(y_test, y_pred)
+# matriz de confusao para resultados de teste e previsoes
+print(confusion_matrix(y_test, previsoes))
+matriz_de_confusao = confusion_matrix(y_test, previsoes)
+# previsão de probabilidades
+probabPrevi = classificador.predict_proba(X_test)[:,1]
 
-# computa probabilidades
-y_pred_prob = classificador.predict_proba(X_test)[:,1]
+
+
 
 novaBase = pd.DataFrame(X_test)
 novaBase['Result'] = y_test
 
-sns.pairplot(novaBase,hue='Result');
-sns.pairplot(novaBase, hue = 'Result', diag_kind = 'kde',plot_kws = {'alpha': 0.6, 's': 80, 'edgecolor': 'k'},size = 4);
+sns.pairplot(novaBase,hue='Result')
+
+'''sns.pairplot(novaBase, hue = 'Result', diag_kind = 'kde',plot_kws = {'alpha': 0.6, 's': 80, 'edgecolor': 'k'},size = 4);
 plt.suptitle('Evolução de pacientes com covide Idade X Fator de risco', 
              size = 15);
 
 sns.scatterplot(data=novaBase, x=0, y=1, hue='Result');
 
-
+plt.scatter(dados['NU_IDADE_N'],dados['EVOLUCAO'])
+plt.plot(x_teste,r,color='red')
+'''
 #sigmoide
 def sigmoide(x):
     return 1/(1+np.exp(-x))
